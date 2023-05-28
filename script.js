b98e40e4e56d4ff29efd49efe55fb51c8f59b6df
Abhishek Srivastava <113105622+AbhishekSrivastava2701@users.noreply.github.com>
2023-05-29 00:36:46 +0530

    Update script.js
    Some changes in the code.

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ let geocode = {
       navigator.geolocation.getCurrentPosition(success, console.error);
     }
     else {
-      weather.fetchWeather("Lucknow");
+      weather.fetchWeather("Kanpur");
     }
   }
 };
@@ -107,7 +107,7 @@ document
     }
   });
 
-weather.fetchWeather("Manipal");
+weather.fetchWeather("Kanpur");
 
 document
   .querySelector(".search-bar")
